a4bb9babfccb1f6bc6f88c452aee70f4a1c03ea2
Ihnat <ya.ihnat@gmail.com>
2014-02-15 19:42:02 +0300

    create first file with small program about solving hanoi puzzle

---
+++ hanoi.js
@@ -0,0 +1,14 @@
+// adopted version of Tower of Hanoi
+// disc - number of discs; src, aux, dst - first, second and third towers.
+// More info about Tower of Hanoi: http://en.wikipedia.org/wiki/Tower_of_Hanoi
+
+var hanoi = function (disc, src, aux, dst) {
+    if (disc > 0) {
+        hanoi(disc - 1, src, dst, aux);
+        document.writeln('<div>Move disc ' + disc +
+                ' from ' + src + ' to ' + dst +'</div>');
+        hanoi(disc - 1, aux, src, dst);
+    }
+}
+
+hanoi(8, 'first', 'second', 'third');
